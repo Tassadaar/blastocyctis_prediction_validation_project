df64ac5a5ef304ac7d7e18a4ecbf85870b02e20d
Jason Shao <qz228113@dal.ca>
2023-06-12 16:10:46 -0300

    motif searching complete

--- false_prediction_validator.py
+++ false_prediction_validator.py
@@ -15,7 +15,8 @@ def main():
     # load fasta
     fasta = SeqIO.index(args.fasta_file, "fasta")
     # parse gff3 file and store in a sqlite3 database
-    db = gffutils.create_db(args.prediction, "test.db", force=True, merge_strategy="merge")
+    db = gffutils.create_db(args.prediction, ":memory:", merge_strategy="merge")
+    motif_count = 0
 
     for gene in db.features_of_type("gene"):
         # this needs input validation: what if valid_ORF is null?
@@ -25,26 +26,40 @@ def main():
             missing_start_codon = gene.attributes["missing_start_codon"]
 
             if missing_start_codon[0] == "True":
-                search_start = None
-                search_end = None
 
                 if gene.strand == "+":
                     search_start = int(gene.end)
                     search_end = search_start + 15
+                    motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
+
+                    if re.search(r"T[ACTG]{2}[ACTG]{2}TGTTTGTT", motif_range):  # check if the motif exists
+                        gene.attributes["motif"] = "True"
+                        motif_count += 1
+                    else:
+                        gene.attributes["motif"] = "False"
+
                 elif gene.strand == "-":
                     search_end = int(gene.start)
                     search_start = search_end - 15
+                    motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
 
-                print(f"{search_start}, {search_end}")
-                motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
-                print(motif_range)
+                    if re.search(r"AACAAACA[ACTG]{2}[ACTG]{2}A", motif_range):  # check if the motif exists
+                        gene.attributes["motif"] = ["True"]
+                        motif_count += 1
+                    else:
+                        gene.attributes["motif"] = ["False"]
 
-    # print the database, write to gff3
-    # for g in db.features_of_type('gene', order_by=('seqid', 'start')):
-    #     print()
-    #     print(g)
-    #     for f in db.children(g, order_by='start'):
-    #         print(f)
+    # write to gff3
+    with open("test.gff3", "w") as out:
+        out.write(f"# There were {motif_count} transferred predictions with the motif found\n")
+        for gene in db.features_of_type("gene", order_by=("seqid", "start")):
+            out.write("\n")
+            out.write(f"{str(gene)}\n")
+            for child in db.children(gene, order_by="start"):
+                out.write(f"{str(child)}\n")
+
+    # status
+    print(f"There were {motif_count} transferred predictions with the motif found")
 
 
 def get_seq(fasta, contig_id, start, end):

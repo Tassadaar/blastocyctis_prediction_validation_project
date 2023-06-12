2c02f72395090df23a029a28c344e089c897a751
Jason Shao <qz228113@dal.ca>
2023-06-12 11:12:14 -0300

    sequence coordinate input complete

--- false_prediction_validator.py
+++ false_prediction_validator.py
@@ -13,10 +13,29 @@ args = parser.parse_args()
 # parse gff3 file and store in a sqlite3 database
 db = gffutils.create_db(args.prediction, "test.db", force=True, merge_strategy="merge")
 
+for gene in db.features_of_type("gene"):
+    # this needs input validation: what if valid_ORF is null?
+    valid_ORF = gene.attributes["valid_ORF"]  # for some reason, this returns a list with a single item, hence index 0
+
+    if valid_ORF[0] == "False":
+        missing_start_codon = gene.attributes["missing_start_codon"]
+
+        if missing_start_codon[0] == "True":
+            search_start = None
+            search_end = None
+
+            if gene.strand == "+":
+                search_start = int(gene.end)
+                search_end = search_start + 15
+            elif gene.strand == "-":
+                search_end = int(gene.start)
+                search_start = search_end - 15
+
+            print(f"{search_start}, {search_end}")
+            
 # print the database, write to gff3
-for g in db.features_of_type('gene', order_by=('seqid', 'start')):
-    print()
-    print(g)
-    for f in db.children(g, order_by='start'):
-        print(f)
-        
+# for g in db.features_of_type('gene', order_by=('seqid', 'start')):
+#     print()
+#     print(g)
+#     for f in db.children(g, order_by='start'):
+#         print(f)
--- run_validator.sh
+++ run_validator.sh
@@ -2,6 +2,6 @@
 
 source activate blastocyctisValidator
 
-python false_prediction_validator.py -a input_files/ST7B_GCA_00151665.1.fna_shortened_headers.fna -p input_files/test.gff3 > output.gff3
+python false_prediction_validator.py -a input_files/ST7B_GCA_00151665.1.fna_shortened_headers.fna -p input_files/test.gff3
 
 conda deactivate

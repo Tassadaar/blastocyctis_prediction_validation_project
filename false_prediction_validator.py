fcf0d6b6746fbcec3b0df6487a8c36d25273e79c
Jason Shao <qz228113@dal.ca>
2023-06-13 16:10:29 -0300

    functionality reached, need clean-up

--- false_prediction_validator.py
+++ false_prediction_validator.py
@@ -18,58 +18,52 @@ def main():
     db = gffutils.create_db(args.prediction, dbfn=":memory:", id_spec=id_func)
     # counter for number of motifs found
     motif_counter = 0
+    output = "test.gff3"
 
-    for gene in db.features_of_type("gene"):
-        # validation
-        if "valid_orf" and "missing_start_codon" not in gene.attributes:
-            continue
+    with open(output, "w") as out:
+        for gene in db.features_of_type("gene"):
+            # validation
+            if "valid_orf" and "missing_start_codon" not in gene.attributes:
+                out.write(f"\n{str(gene)}\n")
+                for child in db.children(gene, order_by="start"):
+                    out.write(f"{str(child)}\n")
+                continue
 
-        valid_orf = gene.attributes["valid_ORF"]  # for some reason this returns a list with a single item
-        missing_start_codon = gene.attributes["missing_start_codon"]
+            valid_orf = gene.attributes["valid_ORF"]  # for some reason this returns a list with a single item
+            missing_start_codon = gene.attributes["missing_start_codon"]
 
-        if valid_orf[0] == "False":
+            if valid_orf[0] == "False":
 
-            if missing_start_codon[0] == "True":
+                if missing_start_codon[0] == "True":
 
-                if gene.strand == "+":
-                    search_start = int(gene.end)
-                    search_end = search_start + 15
-                    motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
+                    if gene.strand == "+":
+                        search_start = int(gene.end)
+                        search_end = search_start + 15
+                        motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
 
-                    if re.search(r"T[ACTG]{2}[ACTG]{2}TGTTTGTT", motif_range):  # check if the motif exists
-                        gene.attributes["motif"] = "True"
-                        motif_counter += 1
-                        print(gene)
-                    else:
-                        gene.attributes["motif"] = "False"
+                        if re.search(r"T[ACTG]{2}[ACTG]{2}TGTTTGTT", motif_range):  # check if the motif exists
+                            gene.attributes["motif"] = "True"
+                            motif_counter += 1
+                        else:
+                            gene.attributes["motif"] = "False"
 
-                elif gene.strand == "-":
-                    search_end = int(gene.start)
-                    search_start = search_end - 15
-                    motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
+                    elif gene.strand == "-":
+                        search_end = int(gene.start)
+                        search_start = search_end - 15
+                        motif_range = get_seq(fasta, gene.seqid, search_start, search_end)
 
-                    if re.search(r"AACAAACA[ACTG]{2}[ACTG]{2}A", motif_range):  # check if the motif exists
-                        gene.attributes["motif"] = ["True"]
-                        motif_counter += 1
-                        print(gene)
-                    else:
-                        gene.attributes["motif"] = ["False"]
+                        if re.search(r"AACAAACA[ACTG]{2}[ACTG]{2}A", motif_range):  # check if the motif exists
+                            gene.attributes["motif"] = ["True"]
+                            motif_counter += 1
+                        else:
+                            gene.attributes["motif"] = ["False"]
 
-        # gene.attributes["ID"] = gene.attributes["ID"][0] + "-1"
+            out.write(f"\n{str(gene)}\n")
+            for child in db.children(gene, order_by="start"):
+                out.write(f"{str(child)}\n")
 
-
-
-    # write to gff3
-    # with open("test.gff3", "w") as out:
-    #     out.write(f"# There were {motif_counter} transferred predictions with the motif found\n")
-    #     for gene in db.features_of_type("gene", order_by=("seqid", "start")):
-    #         out.write("\n")
-    #         out.write(f"{str(gene)}\n")
-    #         for child in db.children(gene, order_by="start"):
-    #             out.write(f"{str(child)}\n")
-
-    # status
-    print(f"\nThere were {motif_counter} transferred predictions with the motif found")
+    # status report
+    prepend_to_file(output, f"# There were {motif_counter} transferred predictions with the motif found")
 
 
 def id_func(x):
@@ -88,6 +82,15 @@ def get_seq(fasta, contig_id, start, end):
     motif_range = str(contig_seq[start - 1:end - 1])  # python slicing is 0-based
 
     return motif_range
+
+
+def prepend_to_file(file_path, text):
+    with open(file_path, 'r') as f:
+        old_content = f.read()
+
+    with open(file_path, 'w') as f:
+        f.write(text + '\n')  # Write the new content first
+        f.write(old_content)  # Write the original content afterwards
 
 
 if __name__ == "__main__":
--- run_validator.sh
+++ run_validator.sh
@@ -4,5 +4,4 @@ source activate blastocyctisValidator
 
 python false_prediction_validator.py -f input_files/ST7B_GCA_00151665.1.fna_shortened_headers.fna -p input_files/1_ST7C_to_ST7B_transfer_additional_orfs.gff3
 
-
 conda deactivate

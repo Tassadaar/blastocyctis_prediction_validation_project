888bf90ddcf3d5694845c3a99dc5d3be6ee2be27
Jason Shao <qz228113@dal.ca>
2023-06-13 15:37:14 -0300

    validation relatively complete, need to finish output

--- false_prediction_validator.py
+++ false_prediction_validator.py
@@ -4,14 +4,6 @@ import gffutils
 from Bio import SeqIO
 
 
-class MotifCounter:
-    def __init__(self):
-        self.count = 0
-
-    def increment(self):
-        self.count += 1
-
-
 def main():
     parser = argparse.ArgumentParser(description="Validates false predictions flagged by liftoff")
 
@@ -23,39 +15,19 @@ def main():
     # load fasta
     fasta = SeqIO.index(args.fasta_file, "fasta")
     # parse gff3 file and store in a sqlite3 database
-    db = gffutils.create_db(args.prediction, ":memory:", merge_strategy="merge")
+    db = gffutils.create_db(args.prediction, dbfn=":memory:", id_spec=id_func)
     # counter for number of motifs found
-    motif_counter = MotifCounter()
+    motif_counter = 0
 
-    db.update(iterator(fasta, db, motif_counter))
-
-    # write to gff3
-    with open("test.gff3", "w") as out:
-        out.write(f"# There were {motif_counter.count} transferred predictions with the motif found\n")
-        for gene in db.features_of_type("gene", order_by=("seqid", "start")):
-            out.write("\n")
-            out.write(f"{str(gene)}\n")
-            for child in db.children(gene, order_by="start"):
-                out.write(f"{str(child)}\n")
-
-    # status
-    print(f"There were {motif_counter.count} transferred predictions with the motif found")
-
-
-def get_seq(fasta, contig_id, start, end):
-    contig_seq = fasta[contig_id].seq.upper()
-    motif_range = str(contig_seq[start - 1:end - 1])  # python slicing is 0-based
-
-    return motif_range
-
-
-def iterator(fasta, db, counter):
     for gene in db.features_of_type("gene"):
-        # this needs input validation: what if valid_ORF is null?
+        # validation
+        if "valid_orf" and "missing_start_codon" not in gene.attributes:
+            continue
+
         valid_orf = gene.attributes["valid_ORF"]  # for some reason this returns a list with a single item
+        missing_start_codon = gene.attributes["missing_start_codon"]
 
         if valid_orf[0] == "False":
-            missing_start_codon = gene.attributes["missing_start_codon"]
 
             if missing_start_codon[0] == "True":
 
@@ -66,7 +38,8 @@ def iterator(fasta, db, counter):
 
                     if re.search(r"T[ACTG]{2}[ACTG]{2}TGTTTGTT", motif_range):  # check if the motif exists
                         gene.attributes["motif"] = "True"
-                        counter.increment()
+                        motif_counter += 1
+                        print(gene)
                     else:
                         gene.attributes["motif"] = "False"
 
@@ -77,13 +50,44 @@ def iterator(fasta, db, counter):
 
                     if re.search(r"AACAAACA[ACTG]{2}[ACTG]{2}A", motif_range):  # check if the motif exists
                         gene.attributes["motif"] = ["True"]
-                        counter.increment()
+                        motif_counter += 1
+                        print(gene)
                     else:
                         gene.attributes["motif"] = ["False"]
 
-        gene.attributes["ID"] = gene.attributes["ID"][0] + "-1"
+        # gene.attributes["ID"] = gene.attributes["ID"][0] + "-1"
 
-        yield gene
+
+
+    # write to gff3
+    # with open("test.gff3", "w") as out:
+    #     out.write(f"# There were {motif_counter} transferred predictions with the motif found\n")
+    #     for gene in db.features_of_type("gene", order_by=("seqid", "start")):
+    #         out.write("\n")
+    #         out.write(f"{str(gene)}\n")
+    #         for child in db.children(gene, order_by="start"):
+    #             out.write(f"{str(child)}\n")
+
+    # status
+    print(f"\nThere were {motif_counter} transferred predictions with the motif found")
+
+
+def id_func(x):
+    new_id = None
+
+    if x.featuretype == 'gene':
+        new_id = x.attributes['ID'][0]
+    elif x.featuretype == 'CDS':
+        new_id = '-'.join([x.attributes['ID'][0], x.seqid, str(x.start), str(x.end)])
+
+    return new_id
+
+
+def get_seq(fasta, contig_id, start, end):
+    contig_seq = fasta[contig_id].seq.upper()
+    motif_range = str(contig_seq[start - 1:end - 1])  # python slicing is 0-based
+
+    return motif_range
 
 
 if __name__ == "__main__":

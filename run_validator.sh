aefebb1050c743ae459b3f7052f7123e57bb2fc3
Jason Shao <qz228113@dal.ca>
2023-06-14 15:57:17 -0300

    made the shell script more readable

--- run_validator.sh
+++ run_validator.sh
@@ -2,6 +2,11 @@
 
 source activate blastocyctisValidator
 
-python false_prediction_validator.py -f input_files/ST7B_GCA_00151665.1.fna_shortened_headers.fna -p input_files/1_ST7C_to_ST7B_transfer_additional_orfs.gff3 -o test.gff3 -m list
+assembly='input_files/ST7B_GCA_00151665.1.fna_shortened_headers.fna'
+prediction='input_files/1_ST7C_to_ST7B_transfer_additional_orfs.gff3'
+output='output.gff3'
+mode='all'
+
+python false_prediction_validator.py -f $assembly -p $prediction -o $output -m $mode
 
 conda deactivate
